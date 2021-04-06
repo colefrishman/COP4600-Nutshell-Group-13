dbc6986ca8a83bb0bea41c95206c444c89dc5313
Cole Frishman <colefrishman@ufl.edu>
2021-04-06 09:48:27 -0400

    environment variable expansion working (kinda)

--- global.h
+++ global.h
@@ -1,5 +1,11 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
 #include <limits.h>
 #include <map>
 #include <string>
 
-std::map<std::string, std::string> aliasTable;
+extern std::map<std::string, std::string> aliasTable;
+
+std::string decipherAlias(std::string aliasText);
+
+#endif
--- helper.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-int run_cd();
-int run_printenv();
-int run_word(char* w);
-int run_setenv(char* var, char* word);
